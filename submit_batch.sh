f83a7579345bab6d6b029c85c237738297a8a748
Argenis Arriojas <arriojasmaldonado001@umb.edu>
2017-11-15 11:25:45 -0500

    Updated submit scritpts

--- submit_batch.sh
+++ submit_batch.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# experiment parameters
 MBS=$1
 BDIST="geometric"
+KB=10
+UM=2
+KP=15
+UP=0.05
+EXPD=150
+
+# jobs parameters
+HL=180      # 3 hours
+NCPU=16
 
 if [ -z $MBS ] || [ ! $MBS -gt 0 ]; then
     echo "ERROR - Need to specify first argument: Mean Burst Size"
@@ -10,11 +20,12 @@ fi
 
 for i in {1..5}
 do
-    bsub -n 16 -R rusage[mem=128] -R span[hosts=1]\
-        -W 4:00 -q short -J $(printf "%02d" $MBS)${BDIST:0:2}$i \
+    bsub -n $NCPU -R rusage[mem=64] -R span[hosts=1]\
+        -W $HL -q short -J $(printf "%02d" $MBS)${BDIST:0:2}i$i \
         python sim.py \
-        -kb 10 -um 2 -kp 15 -up 0.05 -mbs $MBS \
-        -n 20000 -bsd $BDIST -d 150 \
-        -nfp -q -nj 16 -i $i \
-        -rf results.dat
+        -kb $KB -um $UM -kp $KP -up $UP -mbs $MBS \
+        -n 20000 -bsd $BDIST -d $EXPD \
+        -nfp -q -nj $NCPU -i $i \
+        -rf results_kb${KB}-um${UM}-kp${KP}-up${UP}.dat \
+        -l $((($HL-3)*60))
 done
---
+++ submit_exp_batch.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# experiment parameters
+KB=10
+UM=2
+KP=15
+UP=0.05
+EXPD=150
+
+# jobs parameters
+HL=180      # 3 hours
+NCPU=16
+
+for BDIST in {"conditional_geometric"}
+do
+    for MBS in {1,2,4,6,8,12,16}
+    do
+        for i in {1..5}
+        do
+            bsub -n $NCPU -R rusage[mem=64] -R span[hosts=1]\
+                -W $HL -q short -J $(printf "%02d" $MBS)${BDIST:0:2}i$i \
+                python sim.py \
+                -kb $KB -um $UM -kp $KP -up $UP -mbs $MBS \
+                -n 20000 -bsd $BDIST -d $EXPD \
+                -nfp -q -nj $NCPU -i $i \
+                -rf results_kb${KB}-um${UM}-kp${KP}-up${UP}.dat \
+                -l $((($HL-3)*60))
+        done
+    done
+done
+
+# Don't need delta distrubutions now
+exit 0
+
+for BDIST in {"delta"}
+do
+    for MBS in {1,2}
+    do
+        for i in {1..5}
+        do
+            bsub -n $NCPU -R rusage[mem=64] -R span[hosts=1]\
+                -W $HL -q short -J $(printf "%02d" $MBS)${BDIST:0:2}i$i \
+                python sim.py \
+                -kb $KB -um $UM -kp $KP -up $UP -mbs $MBS \
+                -n 20000 -bsd $BDIST -d $EXPD \
+                -nfp -q -nj $NCPU -i $i \
+                -rf results_kb${KB}-um${UM}-kp${KP}-up${UP}.dat \
+                -l $((($HL-3)*60))
+        done
+    done
+done
