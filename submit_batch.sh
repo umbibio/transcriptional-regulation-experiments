1357f9cb72e91e7770399614579663e2ba6047c4
Argenis Arriojas <arriojasmaldonado001@umb.edu>
2017-11-14 19:48:36 -0500

    Update time limit to simulate larger experiment

--- submit_batch.sh
+++ submit_batch.sh
@@ -10,11 +10,11 @@ fi
 
 for i in {1..5}
 do
-    bsub -n 8 -R rusage[mem=128] -R span[hosts=1]\
-        -W 2:00 -q short -J $(printf "%02d" $MBS)${BDIST:0:2}$i \
+    bsub -n 16 -R rusage[mem=128] -R span[hosts=1]\
+        -W 4:00 -q short -J $(printf "%02d" $MBS)${BDIST:0:2}$i \
         python sim.py \
         -kb 10 -um 2 -kp 15 -up 0.05 -mbs $MBS \
         -n 20000 -bsd $BDIST -d 150 \
-        -nfp -q -nj 8 -i $i \
+        -nfp -q -nj 16 -i $i \
         -rf results.dat
 done
